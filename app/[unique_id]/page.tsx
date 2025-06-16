cf041678fb1fcb920786885518b58b340ce3c229
Bekzhan.Borbekov <bekzhan.borbekov@tele2.kz>
2025-06-16 17:47:22 +0500

    fix(slug): add revalidate and dynamicParams=false

--- app/[unique_id]/page.tsx
+++ app/[unique_id]/page.tsx
@@ -2,7 +2,12 @@ import { getCarsDetailData } from '@/api/getCarDetailData';
 import { getCarsList } from '@/api/getCarsList';
 import React from 'react'
 
-export async function generateStaticParams() { // для ISR (Incremental Static Regeneration)
+export const dynamicParams = false
+
+// для ISR (Incremental Static Regeneration)
+export const revalidate = 3600
+
+export async function generateStaticParams() {
     const { data } = await getCarsList({ _page: "1", _limit: "999999" })
 
     return data.map((d) => ({
